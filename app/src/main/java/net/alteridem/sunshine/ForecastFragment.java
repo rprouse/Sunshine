2d377114875b504bafc95cdd0f7aef7bc53a6595
Rob Prouse <rob@prouse.org>
2014-07-20 14:10:55 -0400

    Refactoring the settings code

--- app/src/main/java/net/alteridem/sunshine/ForecastFragment.java
+++ app/src/main/java/net/alteridem/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package net.alteridem.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -73,7 +75,9 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            new DownloadForecastTask().execute("Hamilton,ON");
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = sharedPreferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+            new DownloadForecastTask().execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/net/alteridem/sunshine/MainActivity.java
+++ app/src/main/java/net/alteridem/sunshine/MainActivity.java
@@ -33,8 +33,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            this.startActivity(intent);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
